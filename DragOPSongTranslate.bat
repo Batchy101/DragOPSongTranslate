9099be24fe27e87375da00424356e56dcf549eea
Batchy101 <casttheraccoon@gmail.com>
2017-03-02 20:02:43 -0800

    Update DragOPSongTranslate.bat

--- DragOPSongTranslate.bat
+++ DragOPSongTranslate.bat
@@ -16,13 +16,17 @@ for /f "tokens=* delims= " %%a in (input.txt) do (
 set /a N+=1
 echo %%a\n>>Part-1-Translate.txt )
 
+::Cut Extra /n
+set Cut=<Part-1-Translate.txt
+echo %Cut:~0,-2% >Part-1-Translate.txt
+
 ::Move all text onto one line
 for /f "delims=" %%a in (Part-1-Translate.txt) do (
 echo/|set /p ="%%a%" )>>Part-2-translate.txt
 
 ::Add DragOP/ModPE formated code.
 set /p Convert2=<Part-2-translate.txt
-set Convereted3=["%songname%", "%Convert2:~0,-2%".split("\n")],
+set Convereted3=["%songname%", "%Convert2%".split("\n")],
 echo %Convereted3%>output.txt )
 
 ::Remove no longer needed files.
