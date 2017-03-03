f18deeb5c844216b83fbacc326979840a1a14524
Batchy101 <casttheraccoon@gmail.com>
2017-03-02 18:03:54 -0800

    Update DragOPSongTranslate.bat

--- DragOPSongTranslate.bat
+++ DragOPSongTranslate.bat
@@ -4,7 +4,8 @@ echo.Put all text into Translate.txt
 echo.Output of translated text will be in Translated.txt
 echo.
 echo.When done adding your song lyrics, press any key to continue
-pause <nul
+pause >nul
+set /p Songname=Songname: 
 @echo off
 setLocal EnableDelayedExpansion
 for /f "tokens=* delims= " %%a in (translate.txt) do (
@@ -13,14 +14,18 @@ echo %%a\n>>Part-1-Translate.txt
 )
 
 for /f "delims=" %%a in (Part-1-Translate.txt) do cmd /c  (>>"Part-2-Translate.txt" echo %%a)
+del Part-1-Translate.txt
 
+::Add ModPE formated code.
 
-for /f "delims=" %%a in (Part-1-Translate.txt) do (
+for /f "delims=" %%a in (Part-2-Translate.txt) do (
 echo/|set /p ="%%a%"
 )>>Part-3-translate.txt
+del Part-2-Translate.txt
+::Add ModPE formated code.
 
 set /p Convert3=<Part-3-translate.txt
 set Convereted4=["%songname%", "%Convert3%".split("\n")],
-echo %Convereted4%>Part-4-Translate.txt
+echo %Convereted4%>Translated.txt
 )
-::["Cast is Life", "I AM SO FUCKING MAD RIGHT NOW!! MAD MAD!!\nYOU HAVE NO IDEA HOW MAD I AM\n".split("\n")],
+del Part-3-Translate.txt
